02442559f1e9c8951cd8deca7508193612e53088
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-17 17:05:07 +0900

    add: utils

---
+++ CurveFitting/CurveFittingUtils.cs
@@ -0,0 +1,126 @@
+﻿using Algebra;
+using DoubleDouble;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CurveFitting {
+    public static class CurveFittingUtils {
+        public static IEnumerable<(int m, int n)> EnumeratePadeDegree(int coef_counts, int degree_delta) {
+            ArgumentOutOfRangeException.ThrowIfLessThan(coef_counts, 4);
+            ArgumentOutOfRangeException.ThrowIfNegative(degree_delta);
+
+            int d = (coef_counts + 1) / 2;
+
+            if (coef_counts % 2 == 0 && d > 0) {
+                yield return (d, d);
+
+                for (int k = 1; k <= degree_delta / 2 && k + 1 < d; k++) {
+                    yield return (d + k, d - k);
+                    yield return (d - k, d + k);
+                }
+            }
+            else if (coef_counts % 2 == 1 && d > 1 && degree_delta > 0) {
+                yield return (d, d - 1);
+                yield return (d - 1, d);
+
+                for (int k = 1; k < (degree_delta + 1) / 2 && k + 2 < d; k++) {
+                    yield return (d + k, d - 1 - k);
+                    yield return (d - 1 - k, d + k);
+                }
+            }
+        }
+
+        public static bool HasLossDigitsPolynomialCoef(Vector coefs, ddouble xmin, ddouble xmax) {
+            if (ddouble.IsZero(coefs[0])) {
+                if (coefs.Dim > 1) {
+                    return HasLossDigitsPolynomialCoef(coefs[1..], xmin, xmax);
+                }
+
+                return false;
+            }
+
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(xmin, 0);
+            ArgumentOutOfRangeException.ThrowIfLessThan(xmax, 0);
+
+            ddouble xnmin = 1, xnmax = 1;
+
+            for (int k = 1; k < coefs.Dim; k++) {
+                xnmin *= xmin;
+                xnmax *= xmax;
+
+                ddouble cnmin = coefs[k] * xnmin;
+                ddouble cnmax = coefs[k] * xnmax;
+
+                if (ddouble.Sign(coefs[0]) * ddouble.Sign(cnmin) < 0 && (cnmin / coefs[0]) < -0.25) {
+                    return true;
+                }
+
+                if (ddouble.Sign(coefs[0]) * ddouble.Sign(cnmax) < 0 && (cnmax / coefs[0]) < -0.25) {
+                    return true;
+                }
+            }
+
+            ddouble ymin = Vector.Polynomial(xmin, coefs);
+            ddouble ymax = Vector.Polynomial(xmax, coefs);
+
+            if (ddouble.Sign(coefs[0]) * ddouble.Sign(ymin) < 0 || (ymin / coefs[0]) < 0.5) {
+                return true;
+            }
+
+            if (ddouble.Sign(coefs[0]) * ddouble.Sign(ymax) < 0 || (ymax / coefs[0]) < 0.5) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static ddouble RelativeError(ddouble expected, ddouble actual) {
+            if (ddouble.IsZero(expected)) {
+                return ddouble.IsZero(actual) ? ddouble.Zero : ddouble.PositiveInfinity;
+            }
+            else {
+                ddouble error = ddouble.Abs((expected - actual) / expected);
+
+                return error;
+            }
+        }
+
+        public static ddouble AbsoluteError(ddouble expected, ddouble actual) {
+            ddouble error = ddouble.Abs(expected - actual);
+
+            return error;
+        }
+
+        public static ddouble MaxRelativeError(Vector expected, Vector actual) {
+            return Vector.Func(expected, actual, RelativeError).Select(item => item.val).Max();
+        }
+
+        public static ddouble MaxAbsoluteError(Vector expected, Vector actual) {
+            return Vector.Func(expected, actual, AbsoluteError).Select(item => item.val).Max();
+        }
+
+        public static IEnumerable<(ddouble numer, ddouble denom)> EnumeratePadeCoef(Vector param, int m, int n) {
+            if (param.Dim != checked(m + n)) {
+                throw new ArgumentException("invalid param dims", nameof(param));
+            }
+
+            if (m >= n) {
+                for (int i = 0; i < n; i++) {
+                    yield return (param[..m][i], param[m..][i]);
+                }
+                for (int i = 0; i < m - n; i++) {
+                    yield return (param[..m][n + i], 0);
+                }
+            }
+            else {
+                for (int i = 0; i < m; i++) {
+                    yield return (param[..m][i], param[m..][i]);
+                }
+                for (int i = 0; i < n - m; i++) {
+                    yield return (0, param[m..][m + i]);
+                }
+            }
+        }
+    }
+}
---
+++ CurveFittingTest/CurveFittingUtilsTests.cs
@@ -0,0 +1,150 @@
+﻿using Algebra;
+using DoubleDouble;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace CurveFitting.Tests {
+    [TestClass()]
+    public class CurveFittingUtilsTests {
+        [TestMethod()]
+        public void EnumeratePadeDegreeTest() {
+            for (int coef_counts = 4; coef_counts <= 16; coef_counts++) {
+                for (int degree_delta = 0; degree_delta <= 8; degree_delta++) {
+                    Console.WriteLine($"{nameof(coef_counts)} = {coef_counts}");
+                    Console.WriteLine($"{nameof(degree_delta)} = {degree_delta}");
+
+                    foreach ((int m, int n) in CurveFittingUtils.EnumeratePadeDegree(coef_counts, degree_delta)) {
+                        Console.WriteLine($"{m},{n}");
+
+                        Assert.AreEqual(coef_counts, m + n);
+                        Assert.IsTrue(Math.Abs(m - n) <= degree_delta);
+                        Assert.IsTrue(m > 1);
+                        Assert.IsTrue(n > 1);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void HasLossDigitsPolynomialCoefTest() {
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(Vector.Zero(8), -0.25, 0.25));
+
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1), 0, 0.26));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1), -0.26, 0));
+
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(0, 1, -1), 0, 0.26));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(0, 1, -1), -0.26, 0));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1), 0, 0.24));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1), -0.24, 0));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(0, 1, -1), 0, 0.24));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(0, 1, -1), -0.24, 0));
+
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 0, -3), 0, 0.3));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 0, -2), 0, 0.3));
+
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 0, -3), -0.3, 0));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 0, -2), -0.3, 0));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, -1 / 32d, -1 / 64d), 0, 2));
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1.1 / 8d, -1.1 / 16d, -1.1 / 32d), 0, 2));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, -1 / 32d, -1 / 64d, -0.9 / 128d), 0, 2));
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, -1 / 32d, -1 / 64d, -1.1 / 128d), 0, 2));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, 1 / 32d, 1 / 64d), 0, 2));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1.1 / 8d, 1.1 / 16d, 1.1 / 32d), 0, 2));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, 1 / 32d, 1 / 64d, 0.9 / 128d), 0, 2));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, 1 / 32d, 1 / 64d, 1.1 / 128d), 0, 2));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, 1 / 32d, -1 / 64d), -2, 0));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1.1 / 8d, 1.1 / 16d, -1.1 / 32d), -2, 0));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, 1 / 32d, -1 / 64d, 0.9 / 128d), -2, 0));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, -1 / 16d, 1 / 32d, -1 / 64d, 1.1 / 128d), -2, 0));
+
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, -1 / 32d, 1 / 64d), -2, 0));
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1.1 / 8d, -1.1 / 16d, 1.1 / 32d), -2, 0));
+            Assert.IsFalse(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, -1 / 32d, 1 / 64d, -0.9 / 128d), -2, 0));
+            Assert.IsTrue(CurveFittingUtils.HasLossDigitsPolynomialCoef(new Vector(1, 1 / 16d, -1 / 32d, 1 / 64d, -1.1 / 128d), -2, 0));
+        }
+
+        [TestMethod()]
+        public void RelativeErrorTest() {
+            Assert.AreEqual(0, CurveFittingUtils.RelativeError(0, 0));
+
+            Assert.IsTrue(ddouble.IsPositiveInfinity(CurveFittingUtils.RelativeError(0, 1)));
+
+            Assert.AreEqual(1, CurveFittingUtils.RelativeError(1, 0));
+
+            Assert.AreEqual(0.5, CurveFittingUtils.RelativeError(1, 1.5));
+            Assert.AreEqual(0.25, CurveFittingUtils.RelativeError(2, 1.5));
+
+            Assert.AreEqual(0.5, CurveFittingUtils.RelativeError(-1, -1.5));
+            Assert.AreEqual(0.25, CurveFittingUtils.RelativeError(-2, -1.5));
+        }
+
+        [TestMethod()]
+        public void AbsoluteErrorTest() {
+            Assert.AreEqual(0, CurveFittingUtils.AbsoluteError(0, 0));
+
+            Assert.AreEqual(1, CurveFittingUtils.AbsoluteError(0, 1));
+
+            Assert.AreEqual(1, CurveFittingUtils.AbsoluteError(1, 0));
+
+            Assert.AreEqual(0.5, CurveFittingUtils.AbsoluteError(1, 1.5));
+            Assert.AreEqual(0.5, CurveFittingUtils.AbsoluteError(2, 1.5));
+
+            Assert.AreEqual(0.5, CurveFittingUtils.AbsoluteError(-1, -1.5));
+            Assert.AreEqual(0.5, CurveFittingUtils.AbsoluteError(-2, -1.5));
+        }
+
+        [TestMethod()]
+        public void MaxRelativeErrorTest() {
+            Assert.AreEqual(1, CurveFittingUtils.MaxRelativeError(new Vector(2, 3, 4), new Vector(4, 3, 2)));
+        }
+
+        [TestMethod()]
+        public void MaxAbsoluteErrorTest() {
+            Assert.AreEqual(2, CurveFittingUtils.MaxAbsoluteError(new Vector(2, 3, 4), new Vector(4, 3, 2)));
+        }
+
+        [TestMethod()]
+        public void EnumeratePadeCoefTest() {
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 4), (2, 5), (3, 6) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5, 6), 3, 3).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 5), (2, 6), (3, 0), (4, 0) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5, 6), 4, 2).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 3), (2, 4), (0, 5), (0, 6) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5, 6), 2, 4).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 6), (2, 0), (3, 0), (4, 0), (5, 0) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5, 6), 5, 1).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 2), (0, 3), (0, 4), (0, 5), (0, 6) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5, 6), 1, 5).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 5), (2, 0), (3, 0), (4, 0) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5), 4, 1).ToArray()
+            );
+
+            CollectionAssert.AreEqual(
+                new (ddouble, ddouble)[] { (1, 2), (0, 3), (0, 4), (0, 5) },
+                CurveFittingUtils.EnumeratePadeCoef(new Vector(1, 2, 3, 4, 5), 1, 4).ToArray()
+            );
+        }
+    }
+}
