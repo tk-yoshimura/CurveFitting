e93e60276ff23686b7b2ebe577b1aed1f8bdbcd6
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-17 18:58:11 +0900

    fix: robust

--- CurveFitting/RobustPadeFitter.cs
+++ CurveFitting/RobustPadeFitter.cs
@@ -13,6 +13,10 @@ namespace CurveFitting {
 
         /// <summary>フィッティング</summary>
         public Vector ExecuteFitting(int iter = 8, ddouble? norm_cost = null, double eps = 1e-16) {
+            if (!(eps > 0)) {
+                throw new ArgumentOutOfRangeException(nameof(eps));
+            }
+
             double err_threshold, inv_err;
             double[] weights = new double[Points], errs = new double[Points];
 
@@ -22,6 +26,7 @@ namespace CurveFitting {
                 weights[i] = 1;
             }
 
+            double s = 4;
             while (iter > 0) {
                 Vector err = Error(coef);
                 for (int i = 0; i < Points; i++) {
@@ -31,26 +36,23 @@ namespace CurveFitting {
                 double[] sort_err_list = (double[])errs.Clone();
                 Array.Sort(sort_err_list);
 
-                err_threshold = sort_err_list[Points / 2] * 1.25;
+                err_threshold = sort_err_list[Points / 2] * s;
                 if (err_threshold <= eps) {
                     break;
                 }
 
-                inv_err = 1 / err_threshold;
+                inv_err = 1 / (err_threshold + double.Epsilon);
 
                 for (int i = 0; i < Points; i++) {
-                    if (errs[i] >= err_threshold) {
-                        weights[i] = 0;
-                    }
-                    else {
-                        double r = (1 - errs[i] * inv_err);
-                        weights[i] = r * r;
-                    }
+                    double v = errs[i] * inv_err;
+                    double r = double.Max(0d, 1 - v * v);
+                    weights[i] = r * r;
                 }
 
                 coef = base.ExecuteFitting(new Vector(weights), norm_cost);
 
                 iter--;
+                s = double.Max(s * 0.75, 1.25);
             }
 
             return coef;
