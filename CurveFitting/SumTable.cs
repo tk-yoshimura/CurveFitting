b659d346165ff9c32ddb22ce49b7c1e65334b090
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-14 19:48:37 +0900

    fix: thread safe

--- CurveFitting/SumTable.cs
+++ CurveFitting/SumTable.cs
@@ -1,12 +1,13 @@
 ﻿using Algebra;
 using DoubleDouble;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace CurveFitting {
     public class SumTable {
         private readonly List<Vector> xs = [], ys = [];
-        private Dictionary<(int xn, int yn), ddouble> table;
+        private ConcurrentDictionary<(int xn, int yn), ddouble> table;
 
         private Vector? w = null;
 
@@ -20,9 +21,8 @@ namespace CurveFitting {
 
             this.xs.Add(x);
             this.ys.Add(y);
-            this.table = new() {
-                { (0, 0), x.Dim },
-            };
+            this.table = new();
+            this.table[(0, 0)] = x.Dim;
 
             this.X = x;
             this.Y = y;
@@ -34,21 +34,23 @@ namespace CurveFitting {
                     throw new ArgumentOutOfRangeException($"{nameof(xn)},{nameof(yn)}");
                 }
 
-                for (int i = xs.Count; i < xn; i++) {
-                    int xn0 = (i + 1) / 2 - 1, xn1 = i - xn0 - 1;
+                lock (xs) {
+                    for (int i = xs.Count; i < xn; i++) {
+                        int xn0 = (i + 1) / 2 - 1, xn1 = i - xn0 - 1;
 
-                    xs.Add(xs[xn0] * xs[xn1]);
+                        xs.Add(xs[xn0] * xs[xn1]);
+                    }
                 }
 
-                for (int i = ys.Count; i < yn; i++) {
-                    int yn0 = (i + 1) / 2 - 1, yn1 = i - yn0 - 1;
+                lock (ys) {
+                    for (int i = ys.Count; i < yn; i++) {
+                        int yn0 = (i + 1) / 2 - 1, yn1 = i - yn0 - 1;
 
-                    ys.Add(ys[yn0] * ys[yn1]);
+                        ys.Add(ys[yn0] * ys[yn1]);
+                    }
                 }
 
-                if (!table.ContainsKey((xn, yn))) {
-                    ddouble s;
-
+                if (!table.TryGetValue((xn, yn), out ddouble s)) {
                     if (xn > 0 && yn > 0) {
                         Vector x = xs[xn - 1], y = ys[yn - 1];
 
@@ -65,10 +67,10 @@ namespace CurveFitting {
                         s = w is null ? y.Sum : (y * w).Sum;
                     }
 
-                    table.Add((xn, yn), s);
+                    table[(xn, yn)] = s;
                 }
 
-                return table[(xn, yn)];
+                return s;
             }
         }
 
@@ -84,9 +86,8 @@ namespace CurveFitting {
                 }
 
                 this.w = value;
-                this.table = new() {
-                    { (0, 0), w is null ? xs[0].Dim : w.Sum },
-                };
+                this.table = new();
+                this.table[(0, 0)] = w is null ? xs[0].Dim : w.Sum;
             }
         }
     }
